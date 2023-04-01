255d21140c15d0679647b4cc81266bef001caa4f
arthurqueiroz4 <arthurqueiroz@great.ufc.br>
2023-04-01 10:51:44 -0300

    refatorando controller cliente

--- src/main/java/br/com/curso_spring/rest/controller/ClienteController.java
+++ src/main/java/br/com/curso_spring/rest/controller/ClienteController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.data.domain.ExampleMatcher.StringMatcher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.server.reactive.HttpHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,7 +18,9 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import br.com.curso_spring.domain.entity.Cliente;
 import br.com.curso_spring.domain.repository.Clientes;
@@ -30,39 +34,36 @@ public class ClienteController {
 	
 	//OBTER DADOS DO FRONT -> GET
 	@GetMapping("{id}")
-	public ResponseEntity<Cliente> getClienteById(@PathVariable("id") Integer id) {
-		Optional<Cliente> cliente = clientes.findById(id);
-		if (cliente.isPresent()) {
-			
-			return ResponseEntity.ok(cliente.get());
-		}
-		
-		return ResponseEntity.notFound().build();
+	public Cliente getClienteById(@PathVariable("id") Integer id) {
+		return clientes
+					.findById(id)
+					.orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente nao encontrado"));
 	}
 	
 	//SALVAR RECURSO NO SERVIDOR / QUANDO O RECURSO NAO EXISTE NO SERVIDOR
-	@PostMapping("salvar")
-	public ResponseEntity<Cliente> save(@RequestBody Cliente cliente) {
-		Cliente clientesalvo = clientes.save(cliente);
-		return ResponseEntity.ok(clientesalvo);
-		
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)//Http correto quando cria algo no servidor
+	public Cliente save(@RequestBody Cliente cliente) {
+		return clientes.save(cliente);
 	}
 	
 	@DeleteMapping("{id}")
-	public ResponseEntity<Cliente> delete(@PathVariable("id") Integer id) {
-		Optional<Cliente> cliente = clientes.findById(id);
-		if (cliente.isPresent()) {
-			clientes.delete(cliente.get());
-			return ResponseEntity.noContent().build(); //codigo de status de sucesso porem nao é necessário retonar algo
-		}
-		
-		return ResponseEntity.notFound().build();
+	@ResponseStatus(HttpStatus.NO_CONTENT)//codigo de status de sucesso porem nao é necessário retonar algo
+	public void delete(@PathVariable("id") Integer id) {
+		clientes.findById(id)
+			.map(cliente -> {
+				clientes.delete(cliente);
+				return cliente;
+			})
+			.orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente nao encontrado"));
+			
 	}
 	
 	@PutMapping("{id}")
-	public ResponseEntity<Object> update(@PathVariable("id") Integer id,
+	@ResponseStatus(HttpStatus.NO_CONTENT)//codigo de status de sucesso porem nao é necessário retonar algo
+	public void update(@PathVariable("id") Integer id,
 											@RequestBody Cliente cliente){
-		return clientes
+		clientes
 				.findById(id) //esse metodo retorna um optional
 				.map(clienteExistente -> {	//se tiver valor presente, retorna um optional 
 											//com o resultado da funcao passada
@@ -70,20 +71,21 @@ public class ClienteController {
 															//preenche com o id que ja existia daquele
 															//usuario
 					clientes.save(cliente);
-					return ResponseEntity.noContent().build();
-				}).orElseGet(()-> ResponseEntity.notFound().build());//se o valor estiver presente, retorna o valor, senao
+					return cliente;
+				}).orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente nao encontrado"));
+																	//se o valor estiver presente, retorna o valor, senao
 																	//retorna o parametro passado
 	}
 	
 	@GetMapping
-	public ResponseEntity<Object> find (Cliente filtro){
+	@ResponseStatus(HttpStatus.ACCEPTED)
+	public List<Cliente> find (Cliente filtro){
 		ExampleMatcher matcher = ExampleMatcher.matching().withIgnoreCase()					 //permite algumas configurações
 												.withStringMatcher(StringMatcher.CONTAINING);//como o ignoreCase e a forma de
 																							//pesquisar a string.
 		Example<Cliente> example = Example.of(filtro, matcher);// pega as propriedades que estao populadas 
 																//no parametro e cria um example
-		List<Cliente> lista = clientes.findAll(example); 
-		return ResponseEntity.ok(lista);
+		return clientes.findAll(example); 
 																
 	}
 	
